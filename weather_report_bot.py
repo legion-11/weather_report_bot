a09529ee29c1b631bcf7ebc2b379855aba216f6f
levovit <playersoft1999@gmail.com>
2019-07-24 21:12:44 +0300

    procfile fix

--- weather_report_bot.py
+++ weather_report_bot.py
@@ -1,3 +1,4 @@
+import os
 from app import app, db
 from app.models import User
 
@@ -5,3 +6,7 @@ from app.models import User
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
